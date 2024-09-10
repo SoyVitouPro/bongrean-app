3e3b38411b66e6445b28609e0002a387613ba09a
SoyVitouPro <vitoupro.vtp@gmail.com>
2024-09-10 20:32:11 +0700

    'update

--- bongrean_main/landpage/models.py
+++ bongrean_main/landpage/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    # Add custom fields for the user model
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+
+    # Add related_name arguments to resolve clashing reverse accessors
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='landpage_user_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='landpage_user_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+
+    def __str__(self):
+        return self.username
--- bongrean_main/landpage/views.py
+++ bongrean_main/landpage/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
+from dotenv import load_dotenv # type: ignore
+
+# Load environment variables
+load_dotenv()
+
 # Create your views here.
 def home(request):
+    db_name = os.getenv('DB_NAME')
+    print(f"DB_NAME: {db_name}")
+    
     return render(request, 'home.html')
