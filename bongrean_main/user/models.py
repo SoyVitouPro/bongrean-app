52f589aced48fef4d8a3e6b9d11a4c22a482d430
Raccoon <vitoupro.vtp@gmail.com>
2024-09-15 15:13:43 +0700

    update model
    make chagnfe

--- bongrean_main/user/models.py
+++ bongrean_main/user/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,3 +13,12 @@ class Profile(models.Model):
     class Meta:
         # Remove the UniqueConstraint as MySQL doesn't support it with conditions
         pass
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
