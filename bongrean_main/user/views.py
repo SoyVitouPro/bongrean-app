c34bb6965a484c00a46c082e3ddeef49fb66ce76
Ikhode <132768132+MyKhode@users.noreply.github.com>
2024-09-15 14:46:50 +0700

    login sucess with google

--- bongrean_main/user/views.py
+++ bongrean_main/user/views.py
@@ -5,6 +5,27 @@ from django.contrib.auth import logout
 def login(request):
     return render(request, 'login.html')
 # Create your views here.
-def logout(request):
+def logout_view(request):
     logout(request)
     return redirect('/')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+# in your view, e.g., user/views.py
+from allauth.socialaccount.models import SocialAccount
+
+def profile(request):
+    user = request.user
+    profile_picture_url = None
+
+    if user.is_authenticated:
+        try:
+            social_account = SocialAccount.objects.get(user=user, provider='google')
+            extra_data = social_account.extra_data
+            profile_picture_url = extra_data.get('picture')
+        except SocialAccount.DoesNotExist:
+            pass
+
+    return render(request, 'profile.html', {'profile_picture_url': profile_picture_url})
